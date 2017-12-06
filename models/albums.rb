a182f950c03ac1a55df1a2b11675565d83bc8c54
Mark Blanford <markytorture@hotmail.com>
2017-12-06 15:53:47 +0000

    set up artist and album classes

---
+++ models/albums.rb
@@ -0,0 +1,16 @@
+require_relative("../db/sql_runner.rb")
+
+class Albums
+
+  attr_reader :id, :artist_id
+  attr_accessor :title, :genre
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @title = options['title']
+    @genre = options['genre']
+    @artist_id = options['artist_id'].to_i
+  end
+
+
+end
